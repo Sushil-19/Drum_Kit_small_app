0abaf4022a1a8c433a7da2f113d2c83464f0cb68
Sushil-19 <dubesushil19007@gmail.com>
2024-02-28 09:14:02 +0000

    added listeners

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+
+for (var i=0;i<document.querySelectorAll(".drum").length;i++){
+
+    document.querySelector("button")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    });
+
+}
+
